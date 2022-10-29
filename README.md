c67034141b32d95a6c3443b6f610e320c8f36c0c
Edmund Sagoe <edsagoefvr@gmail>
2022-10-29 13:07:45 +0000

    Pseudocode

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # password_gen
-Applying basic Python knowledge to program a password generator
+
+## Pseudocode
+1. Make arrays of numbers (0 - 9), lowercase alphabet, uppercase alphabet and symbols
+2. Use random to so select at least one character from each of array
+3. combine all the arrays and store in a variable
+4. 
---
+++ gen.py
@@ -0,0 +1,32 @@
+import random, array
+
+maxLen = 8
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+lowerAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+upperAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+symbols = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '|', '/', '>', '<']
+
+combined = numbers + lowerAlphabet + upperAlphabet + symbols
+
+rand_num = random.choice(numbers)
+rand_lower = random.choice(lowerAlphabet)
+rand_upper = random.choice(upperAlphabet)
+rand_sym = random.choice(symbols)
+
+tmp = rand_num + rand_lower + rand_upper + rand_sym
+
+for i in range(maxLen - 4):
+    tmp += random.choice(combined)
+    # convert tmp into an array and shuffle
+    tmp_list = array.array('u', tmp)
+    random.shuffle(tmp_list)
+
+# traverse array
+password = ""
+for i in tmp_list:
+    password += i
+
+print(password)
